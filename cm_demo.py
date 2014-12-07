ed14dcc54c634e00e4c7a4d207fb0f9d7373d2ce
David Edmundson <kde@davidedmundson.co.uk>
2014-12-07 23:17:53 +0100

    Add initial files

---
+++ cm_demo.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+
+import telepathy
+import gobject
+import dbus
+
+from dbus.mainloop.glib import DBusGMainLoop
+
+import sys
+
+class HangupsConnectionManager(telepathy.server.ConnectionManager):
+    def __init__(self):
+        print ("Making manager")
+        telepathy.server.ConnectionManager.__init__(self, 'hangups')
+        self._implement_protocol('hangouts', HangupsProtocol) #protocol is still hangouts, _NOT_ hangups
+
+
+class HangupsProtocol(telepathy.server.Protocol,
+                      telepathy.server.ProtocolInterfacePresence):
+    _proto_ = "hangouts"
+    _englishName = "Google Hangouts"
+    _vcard_field = ""
+    _icon = "" #FIXME
+
+
+    _mandatory_parameters = {"account" : "s"}
+
+    _supported_interfaces = [
+            telepathy.CONNECTION_INTERFACE_ALIASING,
+            telepathy.CONNECTION_INTERFACE_AVATARS,
+            telepathy.CONNECTION_INTERFACE_SIMPLE_PRESENCE,
+            telepathy.CONNECTION_INTERFACE_CONTACTS
+        ]
+
+    _statuses = {
+            "A":(
+                telepathy.CONNECTION_PRESENCE_TYPE_AVAILABLE,
+                True, True),
+            "B":(
+                telepathy.CONNECTION_PRESENCE_TYPE_OFFLINE,
+                True, False)
+            }
+
+    #claim to support 1-1 text messages
+    _requestable_channel_classes = [
+        ({telepathy.CHANNEL_INTERFACE + '.ChannelType': dbus.String(telepathy.CHANNEL_TYPE_TEXT),
+          telepathy.CHANNEL_INTERFACE + '.TargetHandleType': dbus.UInt32(telepathy.HANDLE_TYPE_CONTACT)},
+         [telepathy.CHANNEL_INTERFACE + '.TargetHandle',
+          telepathy.CHANNEL_INTERFACE + '.TargetID'])]
+
+
+    def __init__(self, connection_manager):
+        print ("Making protocol")
+        telepathy.server.Protocol.__init__(self, connection_manager, 'hangouts')
+        telepathy.server.ProtocolInterfacePresence.__init__(self)
+
+    def create_connection(self, connection_manager, parameters):
+        return HangupsConnection(self, connection_manager, parameters)
+
+    def NormalizeContact(self, contactId):
+        return "BLAH"+contactId
+
+
+class HangupsConnection(telepathy.server.Connection,
+        telepathy.server.ConnectionInterfaceRequests): #all other Ifaces here too
+
+    def __init__(self, protocol, manager, parameters):
+        print ("Making Connection")
+        protocol.check_parameters(parameters)
+        account = unicode(parameters['account'])
+
+        telepathy.server.Connection.__init__(self, 'hangouts', account,
+                'hangups', protocol)
+        telepathy.server.ConnectionInterfaceRequests.__init__(self)
+
+
+        #lets maek a channel?
+        #c = HangupsTextChannel(self, )
+
+    def Connect(self):
+        print ("asked to connect")
+
+    def Disconnect(self):
+        print ("asked to disconnect")
+
+class HangupsChannelManager(telepathy.server.ChannelManager):
+    def __init__(self, connection, protocol):
+        telepathy.server.ChannelManager.__init__(self, connection)
+        self.set_requestable_channel_classes(protocol.requestable_channels)
+        self.implement_channel_classes(telepathy.CHANNEL_TYPE_TEXT, self._get_text_channel)
+
+    def _get_text_channel(self, props):
+        print "Making new text channel"
+
+
+
+class HangupsTextChannel(telepathy.server.ChannelTypeText,
+        telepathy.server.ChannelInterfaceChatState):
+
+    #TODO probably pass the conversation object from Hangups client
+    def __init__(self, conn, manager, props, object_path=None):
+        telepathy.server.ChannelTypeText.__init__(self, conn, manager, props,
+                    object_path=object_path)
+
+        self._implement_property_get(CHANNEL_INTERFACE_MESSAGES, {
+            'SupportedContentTypes': lambda: ["text/plain"] ,
+            'MessagePartSupportFlags': lambda: 0,
+            'DeliveryReportingSupport': lambda: telepathy.DELIVERY_REPORTING_SUPPORT_FLAG_RECEIVE_FAILURES,
+            'PendingMessages': lambda: dbus.Array(self._pending_messages2.values(), signature='aa{sv}')
+            })
+
+        self._add_immutables({
+            'SupportedContentTypes': CHANNEL_INTERFACE_MESSAGES,
+            'MessagePartSupportFlags': CHANNEL_INTERFACE_MESSAGES,
+            'DeliveryReportingSupport': CHANNEL_INTERFACE_MESSAGES,
+            })
+
+if __name__ == '__main__':
+
+    #this is a collection of black magic.
+    dbus.set_default_main_loop(dbus.mainloop.glib.DBusGMainLoop())
+    manager = HangupsConnectionManager()
+    mainloop = gobject.MainLoop(is_running=True)
+    mainloop.run()
---
+++ hangups_demo.py
@@ -0,0 +1,26 @@
+#!/bin/python
+import hangups
+import asyncio #maybe want to use gobject event loop? depends on python+dbus
+
+user_list = None
+client = None
+
+def _on_connect(initial_data):
+    print ("connected")
+    user_list = hangups.UserList(
+            client, initial_data.self_entity, initial_data.entities,
+            initial_data.conversation_participants
+        )
+    for x in user_list._user_dict:
+        print (user_list._user_dict[x].full_name)
+        print (user_list._user_dict[x].emails)
+
+
+cookies = hangups.auth.get_auth_stdin("/tmp/foo")
+client = hangups.Client(cookies)
+client.on_connect.add_observer(_on_connect)
+
+print ("about to start main loop")
+client.connect()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(client.connect())
