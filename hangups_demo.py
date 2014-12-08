79bdef0a08977407c281854eca340d240daa1f77
David Edmundson <kde@davidedmundson.co.uk>
2014-12-08 12:46:55 +0100

    Track conversation events

--- hangups_demo.py
+++ hangups_demo.py
@@ -3,7 +3,9 @@ import hangups
 import asyncio #maybe want to use gobject event loop? depends on python+dbus
 from os.path import expanduser
 
+
 user_list = None
+conversation_list = None
 client = None
 
 def _on_connect(initial_data):
@@ -12,10 +14,27 @@ def _on_connect(initial_data):
             client, initial_data.self_entity, initial_data.entities,
             initial_data.conversation_participants
         )
+
+    conversation_list = hangups.ConversationList(
+        client, initial_data.conversation_states, user_list,
+        initial_data.sync_timestamp)
+
+    conversation_list.on_event.add_observer(on_conversation_list_event)
+
     for x in user_list._user_dict:
         print (user_list._user_dict[x].full_name)
         print (user_list._user_dict[x].emails)
 
+    for conv in conversation_list.get_all():
+        print ("active conv", conv.name)
+        conv.on_event.add_observer(on_conversation_event)
+
+def on_conversation_list_event(self, conv_event):
+    if isinstance(conv_event, hangups.ChatMessageEvent):
+        print ("new conversation ")
+
+def on_conversation_event(self, conv_event):
+    print "conversation event"
 
 cookies = hangups.auth.get_auth_stdin(expanduser("~/hangups_auth_tmp"))
 client = hangups.Client(cookies)
